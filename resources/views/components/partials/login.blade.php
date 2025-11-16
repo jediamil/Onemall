d53613971a3486dfa89460fdbcfb42d9ec635261
Jedidiah Amil <iamjediamil.official@gmail.com>
2025-11-16 22:52:06 +0800

    updated

--- resources/views/components/partials/login.blade.php
+++ resources/views/components/partials/login.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full flex justify-center">
-    <img src="{{ asset('images/ONEMALLLOGOFORLIGHTBG.png') }}" alt="ONEMALL" class="w-90 p-8">
+    <img src="{{ asset('images/ONEMALLLOGOFORLIGHTBG.png') }}" alt="ONEMALL" class="w-100 p-8">
 </div>
 
 <div class="w-full flex flex-col items-center p-10">
